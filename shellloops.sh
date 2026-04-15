a8a2fb3f036fcbd4509fa5a8b846717804988f2c
sathivillipraveena <praveenasathivilli1092@gmail.com>
2026-04-15 17:05:32 +0530

    learning to explore for loop

--- shellloops.sh
+++ shellloops.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-echo "enter the number between 1 to 10"
+echo  "enter the number"
 read num
-if [ $num -le 10 ]; then
-    for i in {1..10}
-    do
-        if [ $num -eq $i ]; then
-        echo "$num is in the range of $i"
-        fi
-    done
-else 
-    echo "$num is not in the range please enter 1 to 10 only"
+if [ $((num % 5)) -eq 0 ]; then
+    echo " number $num is divisible by 5 "
+else
+    echo "number $num is not divisible by 5"
 fi
