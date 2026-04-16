a31b707df2e29787c92a99a5bfeba12207f7c842
sathivillipraveena <praveenasathivilli1092@gmail.com>
2026-04-16 12:57:22 +0530

    array

---
+++ array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+movies=("The nun" "conjuring" "anabelle")
+echo "first movie: ${movies[0]}"
+echo "second movie:${movies[1]}"
+echo "all the movies:${movies[@]}"
+echo "movie[4]:${movies[4]}"
