866723dbbb1c783eed0c90f4945d6ed9c30ebf45
sathivillipraveena <praveenasathivilli1092@gmail.com>
2026-04-24 14:47:34 +0530

    installing

--- color.sh
+++ color.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-g= $'\e[32m'
-r=$'\e[31m'
+g=\e[32m
+r=\e[31m
 validate(){
     if [ $1 -ne 0 ] 
     then
-        echo " ${r} ERROR: $2 is not found "
+        echo -e " ${r} ERROR: $2 is not found "
         install $2
     fi
 }
@@ -15,7 +15,7 @@ dnf list installed mysql
 status=$?
 if [ $status -eq 0 ]
 then
-    echo " ${g} mysql is installed"
+    echo -e " ${g} mysql is installed"
     exit 0
 else 
     validate $status "mysql"
