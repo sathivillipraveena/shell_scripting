8d68bd1f690bb2cf090f61bd1809191dd5d1ff67
sathivillipraveena <praveenasathivilli1092@gmail.com>
2026-04-26 12:28:06 +0530

    installing

--- logs.sh
+++ logs.sh
@@ -7,18 +7,18 @@ mkdir -p $LOG_FOLDER
 validate(){
     if [ $1 -ne 0 ] 
     then
-        echo -e " ${r} ERROR: $2 is not found " tee -a logs.log
+        echo -e " ${r} ERROR: $2 is not found "| tee -a $log_file
         install $2
     fi
 }
 install(){
-    dnf install $1 -y & >> logs.log
+    dnf install $1 -y &>> $log_file
 }
-dnf list installed mysql & >> logs.log
+dnf list installed mysql &>> $log_file
 status=$?
 if [ $status -eq 0 ]
 then
-    echo -e " ${g} mysql is installed" tee -a logs.log
+    echo -e " ${g} mysql is installed" |tee -a $log_file
     exit 0
 else 
     validate $status "mysql"
