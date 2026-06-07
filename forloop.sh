6133bd11e623b07e41ff9c7701b3428f24bd3862
sathivillipraveena <praveenasathivilli1092@gmail.com>
2026-06-07 11:28:55 +0530

    roboshop project

---
+++ forloop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+movies=("The nun" "conjuring" "anabelle")
+echo "first movie: ${movies[0]}"
+echo "second movie:${movies[1]}"
+echo "all the movies:${movies[@]}"
+echo "movie[4]:${movies[4]}"
+for i in movies
+do 
+    echo "movies in the array are : ${movies[i]}"
+done
